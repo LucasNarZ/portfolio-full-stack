bd9249c01515780fc279512c66e1fcc5f2eea56b
LucasNarZ <lucasnarlochz@gmail.com>
2023-08-18 10:52:59 -0300

    updating projects page

--- Portfolio/src/pages/projects.jsx
+++ Portfolio/src/pages/projects.jsx
@@ -2,32 +2,27 @@ import "../css/styles.css";
 
 import GithubIcon from '../icons/github-mark.svg';
 
+import LandingPageImage from "../images/landing_page_image.PNG"
+import FormImage from "../images/form_image.PNG"
+
 export function Projects(){
     const projects = [
         {
             title:"Landing Page",
-            image:"",
-            projectLink:"landing-page-iota-mocha.vercel.app",
+            imageLink:LandingPageImage,
+            projectLink:"https://landing-page-iota-mocha.vercel.app",
             description:"The landing page was designed to showcase the product's features and benefits, and to encourage users to take action, such as making a purchase or signing up for a free trial.",
             skills:"Build using: React,  Sass.",
             link:"https://github.com/LucasNarZ/Book-Service-Landing-page.git"
         },
         {
             title:"Form",
-            image:"",
-            projectLink:"form-six-mauve.vercel.app",
+            imageLink:FormImage,
+            projectLink:"https://form-five-xi.vercel.app",
             description:"The objective of this form is to gather information from the user through an intuitive interface.",
             skills:"Build using: Nextjs, Tailwind CSS, Typescript.",
             link:"https://github.com/LucasNarZ/Form.git"
         },
-        {
-            title:"Authentication",
-            image:"",
-            projectLink:"",
-            description:"A full-stack application with sign-up and sign-in systems using Next.js, Tailwind CSS, and Node.js. The application uses PostgreSQL as a database and incorporates numerous security measures to ensure user data privacy and reliability.",
-            skills:"Build using: Nextjs, tailwind CSS, Daisy and Nodejs Express, Typescript.",
-            link:"https://github.com/LucasNarZ/Nodejs-authentication.git"
-        }
     ];
 
     return(
@@ -39,7 +34,7 @@ export function Projects(){
                     <div className="project">
                         <div className="title-image">
                             <p className="project-title">{project.title}</p>
-                            <img src={project.imageLink} className="project-image" alt="" />
+                            <a href={project.projectLink} target="_blank"><img src={project.imageLink} className="project-image" alt="" /></a>
                         </div>
                         <div className="project-info">
                             <p className="project-description">{project.description}</p>
