1a56c2f5e4d6f82da761c046f95480af8527232c
LucasNarZ <lucasnarlochz@gmail.com>
2023-08-20 14:19:43 -0300

    adding footer

--- Portfolio/src/componentes/footer.jsx
+++ Portfolio/src/componentes/footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-
 export function Footer(){
     return(
-        <React.Fragment>
-
-        </React.Fragment>
+        <footer id='footer'>
+            <div className='name-and-prof'>
+                <p className='name'>Lucas</p>
+                <p className='prof'>Front-end Developer</p>
+            </div>
+            <p className='contact-us'>Contact Me</p>
+            <p className='email'>send an email for lucasnarz123@gmail.com</p>
+        </footer>
     )
 }
