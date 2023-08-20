1258e67b755fa509e13f91f0e819cc1c5d1ffabd
LucasNarZ <lucasnarlochz@gmail.com>
2023-08-20 14:25:25 -0300

    removing contact route

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -4,14 +4,13 @@ import './css/styles.css';
 
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { Main } from './pages/Main';
-import { Contact } from './pages/contact';
+
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Main />}/>
-                <Route path="/Contact" element={<Contact />}/>
             </Routes>
         </Router>
     );
